9c0cb56e9b5560cd6c5ecda1b9247cd9cd841a27
tuomas.granlund <tuomas.granlund@mylab.fi>
2019-06-10 00:22:27 +0300

    Added server shutdown hook

--- src/main/java/devgranlund/WebServer.java
+++ src/main/java/devgranlund/WebServer.java
@@ -1,33 +1,43 @@
 package devgranlund;
 
-import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import static io.undertow.Handlers.path;
 
 /**
  * @author tuomas.granlund@gmail.com
  * @since 2019-06-09.
  */
 public class WebServer {
+    
+    private static Undertow server;
 
     public static void main(final String[] args) {
-        Undertow server = Undertow.builder()
+        server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
-                .setHandler(Handlers.path())
-                /*
-                .setHandler(new HttpHandler() {
-                    @Override
-                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
-                    }
-                })
-                 */
-                
+                .setHandler(path()
+                     
+                    // Shutdown
+                    .addPrefixPath("/shutdown", new HttpHandler() {
+                        @Override
+                        public void handleRequest(HttpServerExchange exchange) throws Exception {
+                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                            exchange.getResponseSender().send("Server shutting down... \n");
+                            server.stop();
+                        }
+                    })
+                    // Default handler
+                    .addPrefixPath("/", new HttpHandler() {
+                        @Override
+                        public void handleRequest(HttpServerExchange exchange) throws Exception {
+                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                            exchange.getResponseSender().send("Package listing... \n");
+                        }
+                    })
+                )
                 .build();
         server.start();
     }
-    
 }
