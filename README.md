206d3558de02232dc31ae6941e7373f3707921aa
tuomas.granlund <tuomas.granlund@mylab.fi>
2019-06-09 21:36:30 +0300

    HTTP server basic implementation

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Small program to list currently installed packages
 ### Build
 mvn clean package
 
-### Run 
+### Run web server
+java -jar target/package-viewer-1.0-SNAPSHOT.jar
+
+### Undertow unclean shutdown (Mac)
+lsof -i:8080  
+kill -9 *id
 
 ## Dependencies and rationale
 ### Undertow web server
-Modular, flexible and light-weight HTTP-server implementation.
+Modular, flexible and light-weight HTTP-server implementation.  
+Too much work to do own implementation.
--- src/main/java/devgranlund/WebServer.java
+++ src/main/java/devgranlund/WebServer.java
@@ -1,5 +1,6 @@
 package devgranlund;
 
+import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
@@ -14,13 +15,18 @@ public class WebServer {
     public static void main(final String[] args) {
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
+                .setHandler(Handlers.path())
+                /*
                 .setHandler(new HttpHandler() {
                     @Override
                     public void handleRequest(final HttpServerExchange exchange) throws Exception {
                         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
                         exchange.getResponseSender().send("Hello World");
                     }
-                }).build();
+                })
+                 */
+                
+                .build();
         server.start();
     }
     
